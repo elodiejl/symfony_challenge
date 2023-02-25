f61efb3f2a0060c67f4e815490cfd2644e2cafa5
Ophélie Rochefeuille <ophelie.rochefeuille@green-conseil.com>
2023-02-25 22:48:36 +0100

    Front: API Stripe display balance

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -5,19 +5,28 @@ namespace App\Controller\Admin;
 use App\Repository\OrderRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\MakerBundle\Str;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use Symfony\UX\Chartjs\Model\Chart;
+use Stripe;
+
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
     public function index(OrderRepository $orderRepository, UserRepository $userRepository, ChartBuilderInterface $chartBuilder): Response
     {
+        Stripe\Stripe::setApiKey($_ENV["STRIPE_SECRET"]);
+        $test = Stripe\Balance::retrieve([]);
+        $data = $test['available'];
+        $resourceNotFormatted = $data[0]['amount'];
+        $resource= $resourceNotFormatted/100;
+
         $order = $orderRepository->findAll();
         $ordermensuel = $orderRepository->findByMonth();
-        $gainsAnnuels = 0;
+        $gainsAnnuels = $resource;
         $gainsMensuels = 0;
         $accountsToValidate = $userRepository->findBy(['expectation'=>true]);
         foreach ($order as $o) $gainsAnnuels += $o->getTotalPrice();
